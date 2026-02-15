68af95ae304af9e2eb7a17d487dc51645bb6ba0d
Kundan Kumar <kundanjaiswal6759@gmail.com>
2026-02-15 16:17:15 +0530

    Debug cloudinary env variables

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,6 +1,10 @@
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
+
+console.log("Cloud name:", process.env.CLOUD_NAME);
+console.log("API Key:", process.env.CLOUD_API_KEY);
+console.log("API Secret:", process.env.CLOUD_API_SECRET);
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
